6e53d0e4f90d5667d54ba8cb1a9ccc23899e6951
asari <muhamadasari@smkwikrama.sch.id>
2023-09-13 16:19:19 +0700

    Merge files menjadi satu folder; main

---
+++ main/form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="form-container">
+        <form method="POST" action="backend.php" enctype="multipart/form-data">
+            <label>Nama kegiatan</label>
+            <input type="text" class="form-control" name="namaKegiatan">
+            <label>Nama koordinator</label>
+            <input type="text" class="form-control" name="namaKoordinator">
+            <label>Tempat</label>
+            <input type="text" class="form-control" name="Tempat">
+            <label>Waktu</label>
+            <input type="time" class="form-control waktu-input" name="waktuAwal">
+            <input type="time" class="form-control waktu-input" name="waktuAkhir">
+            <label for="textArea">Note</label>
+            <textarea class="form-control" id="textArea" name="note"></textarea>
+            <label for="image">Upload Image</label>
+            <input type="file" class="form-control" name="image" accept="image/*">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ main/index.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+require 'backend.php';
+
+if (!isset($_SESSION['loggedIn'])) {
+    header("Location: login.php");
+}
+
+if (!isset($_SESSION['index'])) {
+    $_SESSION['index'] = 1;
+}
+
+if (isset($_POST['increment'])) {
+    $_SESSION['index']++;
+} elseif (isset($_POST['decrement']) && $_SESSION['index'] > 1) {
+    $_SESSION['index']--;
+}
+$index = $_SESSION['index'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;1,400&display=swap');
+
+    body {
+        background-color: #ffff;
+        overflow: hidden;
+    }
+
+    .card {
+        background-color: #22668D;
+        height: 43.1rem;
+        width: 25rem !important;
+        border-radius: 0px;
+    }
+
+    .card-body {
+        margin-top: 35px !important;
+        color: #ffff;
+        font-family: 'poppins';
+    }
+
+    .card2 {
+        background-color: #ffff;
+        margin-bottom: 15rem;
+        margin-left: 50px;
+        border-radius: 5px;
+    }
+
+    button {
+        width: 150px;
+        margin-left: 30px;
+    }
+
+    .container {
+        display: flex;
+        padding-right: 50px;
+    }
+</style>
+
+<body>
+    <?php
+    $ambil = "SELECT * from information WHERE id='$index'";
+    $query = mysqli_query($conn, $ambil);
+    if (mysqli_num_rows($query) > 0) {
+        while ($data = mysqli_fetch_assoc($query)) { ?>
+
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <i class="card-text">Kegiatan hari ini</i>
+                    <h1>
+                        <?= $data["ketKegiatan"]; ?>
+                    </h1>
+                </div>
+                <div class="card2" style="width: 18rem;">
+                    <div class="card-body2">
+                        <h5 class="card-title2">Koordinator :
+                            <?= $data["namaKoordinator"]; ?>
+                        </h5>
+                        <h5 class="card-title2">Tempat :
+                            <?= $data["tempat"]; ?>
+                        </h5>
+                        <h5 class="card-title2">Waktu :
+                            <?= $data["waktuAwal"] . "-" . $data["waktuAkhir"]; ?>
+                        </h5>
+                        <p class="card-text2">Note :
+                            <?= $data["note"]; ?>
+                        </p>
+                    </div>
+                </div>
+                <div class="container">
+                    <form action="" method="POST">
+                        <button type="submit" class="btn btn-primary" name="decrement">Sebelumnya</button>
+                        <button type="submit" class="btn btn-primary" name="increment">Selanjutnya</button>
+                    </form>
+                </div>
+            </div>
+        <?php }
+    } ?>
+</body>
+
+</html>
