380d678d5248993e06783a0a5b773e1be67f6e8f
Zananri <symlmrnri17@gmail.com>
2023-09-17 16:14:34 +0700

    Terbaru

--- main/index.php
+++ main/index.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 include 'backend.php';
 
@@ -35,63 +36,83 @@ $_SESSION['index'] = $index;
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css"> <!-- Assuming this is your custom CSS file -->
     <script src="js/script.js" defer></script>
-    
-    <!-- Add the responsive CSS code here -->
-    <style>
-        /* Define styles for small screens (e.g., mobile phones) */
-        @media screen and (max-width: 600px) {
-            body {
-                font-size: 14px;
-            }
-
-            /* Adjust sidebar and main layout */
-            .sidebarContainer {
-                display: flex;
-                flex-direction: column;
-            }
-
-            .main {
-                margin-left: 0;
-                width: 100%;
-            }
-
-            /* Adjust calendar styles */
-            .calendar {
-                font-size: 12px;
-            }
-        }
-
-        /* Define styles for larger screens (e.g., tablets and desktops) */
-        @media screen and (min-width: 601px) {
-            body {
-                font-size: 16px;
-            }
-
-            /* Reset styles to their original values */
-            .sidebarContainer {
-                display: flex;
-                flex-direction: row;
-            }
-
-            .main {
-                margin-left: 220px;
-                width: calc(100% - 220px);
-            }
-
-            /* Reset calendar styles */
-            .calendar {
-                font-size: inherit;
-            }
-        }
-    </style>
 </head>
 
 <body>
-    <!-- Your HTML content remains the same -->
-    <!-- ... -->
+    <section>
+        <div class="sidebarContainer">
+            <!-- <div class="logo">
+            <img src="img/logo.png">
+        </div> -->
+            <div class="sidebarContent">
+                <div class="sidebarText">
+                    <h1><?= date('j F') ?></h1>
+                    <p>Jumlah kegiatan hari ini</p>
+                </div>
+                <div class="sidebarKegiatanBox">
+                    <div class="boxRedContainer">
+                        <div class="box boxRed">7</div>
+                        <p>Penting</p>
+                    </div>
+                    <div class="boxGreenContainer">
+                        <div class="box boxGreen">3</div>
+                        <p>Biasa </p>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <header>
+                        <p class="current-date"></p>
+                        <div class="icons">
+                            <span id="prev" class="material-symbols-rounded">chevron_left</span>
+                            <span id="next" class="material-symbols-rounded">chevron_right</span>
+                        </div>
+                    </header>
+                    <div class="calendar">
+                        <ul class="weeks">
+                            <li>Sun</li>
+                            <li>Mon</li>
+                            <li>Tue</li>
+                            <li>Wed</li>
+                            <li>Thu</li>
+                            <li>Fri</li>
+                            <li>Sat</li>
+                        </ul>
+                        <ul class="days"></ul>
+                    </div>
+                </div>
+                <p><span>Lihat History</span></p>
+            </div>
+            <a href="form.php"><img src="img/AddButton.svg" alt="add button" class="addButtonSvg"></a>
+        </div>
 
+        <div class="main">
+            <?php 
+            $ambil = "SELECT * from information WHERE id='$index'";
+            $query = mysqli_query($conn, $ambil);
+            if (mysqli_num_rows($query) > 0) {
+                while ($data = mysqli_fetch_assoc($query)) { ?>
+            <h2 class="namaKegiatan"><?= $data["ketKegiatan"] ?></h2>
+            <div class="flexKegiatan">
+            <div class="kegiatanContainer">
+                <div class="kegiatanInformation">
+                    <p>Penanggung jawab : <?= $data["penanggungJawab"] ?></p>
+                    <p>Tempat : <?= $data["tempat"] ?></p>
+                    <p>Waktu : <?= $data["waktuAwal"] ?> - <?= $data["waktuAkhir"] ?></p>
+                </div>
+                <div class="kegiatanDescription">
+                    <p><?= $data["deskripsiKegiatan"] ?></p>
+                </div>
+            </div>
+            <img src="<?= $data["img"]?>" alt="gambar kegiatan">
+            <?php } } ?>
+            </div>
+            <form action="" method="GET">
+                <button type="submit" class="controlButton dec" name="decrement" class="sidebarButton">Sebelumnya</button>
+                <button type="submit" class="controlButton inc" name="increment" class="sidebarButton">Selanjutnya</button>
+            </form>
+        </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
