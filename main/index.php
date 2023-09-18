762e3772a98e64742b93d89b5a4a5096a41f9b4f
Zananri <symlmrnri17@gmail.com>
2023-09-18 10:10:01 +0700

    Pembaruan

--- main/index.php
+++ main/index.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 include 'backend.php';
 
@@ -114,5 +115,4 @@ $_SESSION['index'] = $index;
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-
 </html>
